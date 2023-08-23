5b72e341d211ce7b5a896bc1e651c75d00f11b46
Jrm1992 <jose_roberto_6@msn.com>
2023-08-23 03:30:00 -0300

    remove upgrade button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,13 +4,11 @@ import { Poppins } from 'next/font/google';
 import Link from 'next/link';
 
 import { UserButton } from '@clerk/nextjs';
-import { Sparkles } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 
 import MobileSidebar from './mobile-sidebar';
 import { ModeToggle } from './mode-toggle';
-import { Button } from './ui/button';
 
 const font = Poppins({ subsets: ['latin'], weight: '600' });
 
@@ -31,10 +29,6 @@ export default function Navbar() {
         </Link>
       </div>
       <div className="flex items-center gap-x-3">
-        <Button variant={'premium'} size="sm">
-          Upgrade
-          <Sparkles className="ml-2 h-4 w-4 fill-white text-white" />
-        </Button>
         <ModeToggle />
         <UserButton afterSignOutUrl="/" />
       </div>
