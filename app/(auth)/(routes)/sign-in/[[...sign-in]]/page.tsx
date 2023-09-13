687147d63478db4f5c230bb6603304187f6508d7
Jrm1992 <jose_roberto_6@msn.com>
2023-09-13 02:49:32 -0300

    created demo login

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,14 @@
 import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <SignIn
+      appearance={{
+        elements: {
+          // eslint-disable-next-line prettier/prettier
+          form: `before:content-['_Demo_login:_demo@demo.com_________password:_demo'] before:whitespace-pre-wrap before:h-10 before:w-full before:text-xs before:text-black before:leading-relaxed before:block`
+        }
+      }}
+    />
+  );
 }
